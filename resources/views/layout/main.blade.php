bc9e193ef331e04137e36051cf0f77d65b6a614e
Mattia_Cherchi <mattiacherchi93@gmail.com>
2023-02-20 22:13:30 +0100

    scaffolding frontend done

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title ?? config('app.name') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+   
+</head>
+
+<body>
+
+    <x-navbar/>
+   
+
+    {{ $slot }}
+   
+    
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+<x-main>
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-12">
+                <h1>Ciao</h1>
+            </div>
+        </div>
+    </div>
+</x-main>
